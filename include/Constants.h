a0e42e8b6dc8fb303857444ab846df49fbfc4ccb
Zekaroni <ezekiel.hall.04@gmail.com>
2025-08-03 15:21:08 -0400

    added more constants for the program

--- include/Constants.h
+++ include/Constants.h
@@ -2,19 +2,21 @@
 #define CONSTANTS_H
 
 #include <cstdint>
+#include <bitset>
 
 namespace CONSTANTS
 {
-    constexpr uint8_t BOARD_WIDTH  = 10;
-    constexpr uint8_t BOARD_HEIGHT = 20;
-    // constexpr uint8_t BOARD_MAX_HEIGHT = 20;
-    constexpr uint16_t TILE_COUNT = BOARD_HEIGHT * BOARD_WIDTH;
     constexpr uint8_t PIECE_COUNT = 7;
     constexpr uint8_t ROTATION_COUNT = 4;
-
+    constexpr uint8_t BOARD_WIDTH  = 10;
+    constexpr uint8_t BOARD_HEIGHT = 20;
+    constexpr uint16_t TILE_COUNT = BOARD_HEIGHT * BOARD_WIDTH;
     constexpr uint8_t BAG_MIN_LIMIT = 7;
     constexpr uint16_t STARTING_POSITION = 4;
     constexpr uint8_t STARTING_ROTATION = 0;
+    constexpr uint8_t STARTING_GRAVITY = 1;
+    constexpr uint16_t FULL_LINE_MASK = (1 << BOARD_WIDTH) - 1; // Favorite line so far
+    constexpr std::bitset<TILE_COUNT> FULL_BOARD_MASK = (1 << TILE_COUNT) - 1;
 };
 
 #endif
